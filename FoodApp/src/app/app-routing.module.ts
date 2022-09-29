341b160de120509805e0ffd4b09866f0adad8ecf
shishirlb <114382895+shishirlb@users.noreply.github.com>
2022-09-29 14:21:03 +0530

    Bootstrap config and routing

--- FoodApp/src/app/app-routing.module.ts
+++ FoodApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoodProductDetailsComponent } from './BranchManager/FoodProducts/food-product-details/food-product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'food-product-details', component:FoodProductDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FoodApp/src/app/app.module.ts
+++ FoodApp/src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { FoodProductDetailsComponent } from './BranchManager/FoodProducts/food-product-details/food-product-details.component';
+import { AddFoodProductComponent } from './BranchManager/FoodProducts/add-food-product/add-food-product.component';
+import { EditFoodProductComponent } from './BranchManager/FoodProducts/edit-food-product/edit-food-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodProductDetailsComponent,
+    AddFoodProductComponent,
+    EditFoodProductComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
